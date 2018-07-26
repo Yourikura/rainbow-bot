2c0b1eed1e8c7a26fd9ecd66f5f2b263f501ab34
Andrey0189 <39239738+Andrey0189@users.noreply.github.com>
2018-07-26 19:09:39 +0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const forEachTimeout = require('foreach-timeout');
+let prefix = '!'
+client.on('ready', () => {
+    client.user.setActivity(prefix + 'rainbow | ' + client.guilds.size + 'servers',{ type: 'PLAYING' })
+    console.log('Бот запущен успешно\n    Количество гильдий на которых присутствует бот: ' + client.guilds.size);
+});
+bot.on('guildCreate', (guild) => {
+    let channels = guild.channels.filter(channel => channel.type === 'text' && channel.permissionsFor(guild.members.get(bot.user.id)).has('SEND_MESSAGES'));
+    if (channels.size > 0) channels.first().send('Напишите ' + prefix + 'rainbow чтобы навернуть грибов. Вам не нужно создавать роли, вам не нужно ничего настраивать. Бот всё сделает за вас');
+});
+client.on('message', message => {
+    if(message.channel.type !== 'text') return;
+    if(message.channel.id === '469504020323631115') return;
+    if (message.author.bot) return;
+    if(message.content.indexOf(prefix) !== 0) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    if ('rainbow'.includes(command)) {
+        if (!message.guild.roles.find("name", "Rainbow")) {
+            message.guild.createRole({
+                name: 'Rainbow'
+            })
+        }
+        message.channel.send('Роль Rainbow создана и запущена, теперь дайте ее тем участникам которые этой роли достойны');
+        let colors = ["#ff0000", "#ffa500", "#ffff00", "#00ff00", "#00BFFF", "#0000ff", "#ff00ff"];
+        let role = message.guild.roles.find("name", "Rainbow");
+        async function color (colors, role) {
+            forEachTimeout(colors, (color) => {role.setColor(color)}, 1500).then(() => color(colors, role));
+        }
+        color(colors, role);
+    }
+})
